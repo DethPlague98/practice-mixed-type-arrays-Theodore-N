ebc5a4eb89894dc2afe1528ea92dcf7b6207eb1b
DethPlague98 <tnaes98@gmail.com>
2026-04-26 00:58:36 -0500

    initial project

--- mixed-arrays.js
+++ mixed-arrays.js
@@ -1,1 +1,13 @@
-//Task 1
+//Task 1 (Create the Profile)
+let profile = Array(5);
+profile = ["Bob", 72, true, {Location: "Saint Louis"}, ["Baseball", "3D printing", "reading"]];
+
+//Task 2 (Access and Log profile details)
+console.log(profile[0], profile[4][1]);
+
+//Task 3 (Modify the profile)
+profile[1] = 73;
+profile[4][3] = "playing guitar";
+
+//Task 4 (Display the updated profile)
+console.log(profile);
